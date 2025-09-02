c2ce88a676e47bc8736edadff71cee66f187c255
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-09-02 15:18:11 +0300

    rsa

--- lib/src/test/kotlin/sp/kx/secrets/RSAECBEncryptionTest.kt
+++ lib/src/test/kotlin/sp/kx/secrets/RSAECBEncryptionTest.kt
@@ -4,11 +4,35 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.security.KeyPairGenerator
 import java.security.SecureRandom
+import javax.crypto.Cipher
 
 internal class RSAECBEncryptionTest {
     @Test
-    fun toPublicKeyTest() {
-        val encryption: Asymmetric.Encryption = RSAECBEncryption(paddings = "PKCS1Padding")
-        TODO()
+    fun encryptTest() {
+        val generator = KeyPairGenerator.getInstance("RSA")
+        generator.initialize(2048, SecureRandom.getInstanceStrong())
+        val keyPair = generator.generateKeyPair()
+        val decrypted = "foobarbaz".toByteArray()
+        val paddings = "PKCS1Padding"
+        val encryption: Asymmetric.Encryption = RSAECBEncryption(paddings = paddings)
+        val actual = encryption.encrypt(key = keyPair.public, decrypted = decrypted)
+        val cipher = Cipher.getInstance("RSA/ECB/$paddings")
+        cipher.init(Cipher.DECRYPT_MODE, keyPair.private)
+        assertTrue(cipher.doFinal(actual).contentEquals(decrypted))
+    }
+
+    @Test
+    fun decryptTest() {
+        val generator = KeyPairGenerator.getInstance("RSA")
+        generator.initialize(2048, SecureRandom.getInstanceStrong())
+        val keyPair = generator.generateKeyPair()
+        val decrypted = "foobarbaz".toByteArray()
+        val paddings = "PKCS1Padding"
+        val cipher = Cipher.getInstance("RSA/ECB/$paddings")
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.public)
+        val encrypted = cipher.doFinal(decrypted)
+        val encryption: Asymmetric.Encryption = RSAECBEncryption(paddings = paddings)
+        val actual = encryption.decrypt(key = keyPair.private, encrypted = encrypted)
+        assertTrue(decrypted.contentEquals(actual))
     }
 }
