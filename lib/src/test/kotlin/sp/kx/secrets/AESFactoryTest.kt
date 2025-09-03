d17ccca2c350da2255ddc717e7fadfad6601e5ec
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-09-03 12:43:41 +0300

    aes test

--- lib/src/test/kotlin/sp/kx/secrets/AESFactoryTest.kt
+++ lib/src/test/kotlin/sp/kx/secrets/AESFactoryTest.kt
@@ -3,7 +3,9 @@ package sp.kx.secrets
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.security.SecureRandom
+import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
+import javax.crypto.spec.IvParameterSpec
 
 internal class AESFactoryTest {
     @Test
@@ -14,5 +16,21 @@ internal class AESFactoryTest {
         val factory: Symmetric.Factory = Symmetric.AES.factory
         val actual = factory.toSecretKey(encoded = key.encoded)
         assertTrue(key.encoded.contentEquals(actual.encoded))
+    }
+
+    @Test
+    fun newSecretKeyTest() {
+        val factory: Symmetric.Factory = Symmetric.AES.factory
+        val key = factory.newSecretKey()
+        val random: SecureRandom = SecureRandom.getInstanceStrong()
+        val decrypted = "foobarbaz".toByteArray()
+        val paddings = "PKCS5Padding"
+        val iv = ByteArray(16)
+        random.nextBytes(iv)
+        val cipher = Cipher.getInstance("AES/CBC/$paddings")
+        cipher.init(Cipher.ENCRYPT_MODE, key, IvParameterSpec(iv))
+        val encrypted = cipher.doFinal(decrypted)
+        cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(iv))
+        assertTrue(cipher.doFinal(encrypted).contentEquals(decrypted))
     }
 }
