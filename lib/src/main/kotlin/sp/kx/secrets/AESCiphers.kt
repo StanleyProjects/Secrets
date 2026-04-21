6e9bbaed5fbc7ae6552360a180da6d9cd5288d88
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-21 12:41:58 +0300

    aes/cbc/pkcs5padding

--- lib/src/main/kotlin/sp/kx/secrets/AESCiphers.kt
+++ lib/src/main/kotlin/sp/kx/secrets/AESCiphers.kt
@@ -3,6 +3,7 @@ package sp.kx.secrets
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.IvParameterSpec
 
 sealed interface AESCiphers<T : Any> {
     fun encrypt(key: SecretKey, decrypted: ByteArray, specs: T): ByteArray
@@ -27,6 +28,30 @@ sealed interface AESCiphers<T : Any> {
             ): ByteArray {
                 val cipher = Cipher.getInstance("aes/gcm/nopadding")
                 cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(specs.tagSize, specs.iv))
+                return cipher.doFinal(encrypted)
+            }
+        }
+    }
+
+    object CBC {
+        object PKCS5Padding : AESCiphers<IVSpecs> {
+            override fun encrypt(
+                key: SecretKey,
+                decrypted: ByteArray,
+                specs: IVSpecs,
+            ): ByteArray {
+                val cipher = Cipher.getInstance("aes/cbc/pkcs5padding")
+                cipher.init(Cipher.ENCRYPT_MODE, key, IvParameterSpec(specs.iv))
+                return cipher.doFinal(decrypted)
+            }
+
+            override fun decrypt(
+                key: SecretKey,
+                encrypted: ByteArray,
+                specs: IVSpecs,
+            ): ByteArray {
+                val cipher = Cipher.getInstance("aes/cbc/pkcs5padding")
+                cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(specs.iv))
                 return cipher.doFinal(encrypted)
             }
         }
---
+++ lib/src/main/kotlin/sp/kx/secrets/IVSpecs.kt
@@ -0,0 +1,20 @@
+package sp.kx.secrets
+
+class IVSpecs(
+    val iv: ByteArray,
+) {
+    override fun toString(): String {
+        return "IVSpecs(iv:size: ${iv.size})"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is IVSpecs -> iv.contentEquals(other.iv)
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return iv.contentHashCode()
+    }
+}
