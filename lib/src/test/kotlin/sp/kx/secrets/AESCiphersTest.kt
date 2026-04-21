adbf4722a746bb53248d1a2bdb2cf3f4e889707a
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-21 15:43:35 +0300

    test

--- lib/src/test/kotlin/sp/kx/secrets/AESCiphersTest.kt
+++ lib/src/test/kotlin/sp/kx/secrets/AESCiphersTest.kt
@@ -1,29 +1,47 @@
 package sp.kx.secrets
 
-import java.security.SecureRandom
 import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
 import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-internal class AESCiphersTest {
-    @Test
-    fun encryptTest() {
-        val generator = KeyGenerator.getInstance("AES")
-        val seed = ByteArray(32) { 32.minus(it).toByte() }
-        val random = SecureRandom(seed)
-        generator.init(256, random)
-        val key = generator.generateKey()
-        //
-        val iv = ByteArray(12) { 12.minus(it).toByte() }
-        val specs = GCMSpecs(tagSize = 128, iv = iv)
-        //
-        val decrypted = "foobarbaz".toByteArray(Charsets.UTF_8)
-        val encrypted = AESCiphers.GCM.NoPadding.encrypt(key = key, decrypted = decrypted, specs = specs)
-        //
-        val cipher = Cipher.getInstance("aes/gcm/nopadding")
-        cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(specs.tagSize, specs.iv))
-        assertTrue(cipher.doFinal(encrypted).contentEquals(decrypted))
+internal object AESCiphersTest {
+    object GCM {
+        class NoPadding {
+            @Test
+            fun encryptTest() {
+                val expected = "foobarbaz".toByteArray(Charsets.UTF_8)
+                val seed = ByteArray(32) { 32.minus(it).toByte() }
+                val key = SecretKeySpec(seed, "aes")
+                //
+                val iv = ByteArray(12) { 12.minus(it).toByte() }
+                val specs = GCMSpecs(tagSize = 128, iv = iv)
+                //
+                val encrypted = AESCiphers.GCM.NoPadding.encrypt(key = key, decrypted = expected, specs = specs)
+                val cipher = Cipher.getInstance("aes/gcm/nopadding")
+                cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(specs.tagSize, specs.iv))
+                val decrypted = cipher.doFinal(encrypted)
+                //
+                assertTrue(expected.contentEquals(decrypted))
+            }
+
+            @Test
+            fun decryptTest() {
+                val expected = "foobarbaz".toByteArray(Charsets.UTF_8)
+                val seed = ByteArray(32) { 32.minus(it).toByte() }
+                val key = SecretKeySpec(seed, "aes")
+                //
+                val iv = ByteArray(12) { 12.minus(it).toByte() }
+                val specs = GCMSpecs(tagSize = 128, iv = iv)
+                //
+                val cipher = Cipher.getInstance("aes/gcm/nopadding")
+                cipher.init(Cipher.ENCRYPT_MODE, key, GCMParameterSpec(specs.tagSize, specs.iv))
+                val encrypted = cipher.doFinal(expected)
+                val decrypted = AESCiphers.GCM.NoPadding.decrypt(key = key, encrypted = encrypted, specs = specs)
+                //
+                assertTrue(expected.contentEquals(decrypted))
+            }
+        }
     }
 }
