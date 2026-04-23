33c05dd38bb145ef753e1281f22bd843ba52feca
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-23 15:08:56 +0300

    wip

--- lib/src/main/kotlin/sp/kx/secrets/AsyKeys.kt
+++ lib/src/main/kotlin/sp/kx/secrets/AsyKeys.kt
@@ -1,12 +1,17 @@
 package sp.kx.secrets
 
 import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.KeyPairGenerator
 import java.security.PrivateKey
 import java.security.PublicKey
+import java.security.SecureRandom
+import java.security.interfaces.RSAPrivateCrtKey
 import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.RSAPublicKeySpec
 import java.security.spec.X509EncodedKeySpec
 
-class AsyKeys private constructor(val algorithm: String) {
+abstract class AsyKeys private constructor(val algorithm: String) {
     fun toPublicKey(encoded: ByteArray): PublicKey {
         val kf = KeyFactory.getInstance(algorithm)
         return kf.generatePublic(X509EncodedKeySpec(encoded))
@@ -17,8 +22,22 @@ class AsyKeys private constructor(val algorithm: String) {
         return kf.generatePrivate(PKCS8EncodedKeySpec(encoded))
     }
 
-    companion object {
-        val RSA = AsyKeys(algorithm = "rsa")
-        val EC = AsyKeys(algorithm = "ec")
+    object RSA : AsyKeys(algorithm = "rsa") {
+        fun getPublicKey(key: PrivateKey): PublicKey {
+            if (key !is RSAPrivateCrtKey) TODO("AsyKeys:RSA:getPublicKey")
+            val kf = KeyFactory.getInstance("rsa")
+            val spec = RSAPublicKeySpec(key.modulus, key.publicExponent)
+            return kf.generatePublic(spec)
+        }
+
+        fun newKeyPair(random: SecureRandom, keySize: Int): KeyPair {
+            val kpg = KeyPairGenerator.getInstance("rsa")
+            kpg.initialize(keySize, random)
+            return kpg.generateKeyPair()
+        }
+    }
+
+    object EC : AsyKeys(algorithm = "ec") {
+        val SECP256R1 = ECKeys(name = "secp256r1")
     }
 }
---
+++ lib/src/main/kotlin/sp/kx/secrets/ECKeys.kt
@@ -0,0 +1,54 @@
+package sp.kx.secrets
+
+import org.bouncycastle.jce.ECNamedCurveTable
+import java.math.BigInteger
+import java.security.AlgorithmParameters
+import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.SecureRandom
+import java.security.interfaces.ECPrivateKey
+import java.security.spec.ECGenParameterSpec
+import java.security.spec.ECParameterSpec
+import java.security.spec.ECPoint
+import java.security.spec.ECPrivateKeySpec
+import java.security.spec.ECPublicKeySpec
+
+class ECKeys internal constructor(name: String) {
+    private val spec = ECNamedCurveTable.getParameterSpec(name)
+
+    fun newKeyPair(random: SecureRandom): KeyPair {
+        val kpg = KeyPairGenerator.getInstance("ec")
+        kpg.initialize(ECGenParameterSpec(spec.name), random)
+        return kpg.generateKeyPair()
+    }
+
+    fun getPublicKey(key: PrivateKey): PublicKey {
+        if (key !is ECPrivateKey) TODO("ECKeys:getPublicKey")
+        val generator = key.params.generator
+        val p = spec.curve.createPoint(generator.affineX, generator.affineY)
+        val point = spec
+            .curve
+            .multiplier
+            .multiply(p, key.s)
+            .normalize()
+        val w = ECPoint(point.affineXCoord.toBigInteger(), point.affineYCoord.toBigInteger())
+        val kf = KeyFactory.getInstance("ec")
+        return kf.generatePublic(ECPublicKeySpec(w, key.params))
+    }
+
+    fun getPrivateKey(magnitude: ByteArray): PrivateKey {
+        val ap = AlgorithmParameters.getInstance("ec")
+        ap.init(ECGenParameterSpec(spec.name))
+        val params = ap.getParameterSpec(ECParameterSpec::class.java)
+        if (magnitude.size != params.order.bitLength().plus(7).div(8)) {
+            TODO("magnitude:size: ${magnitude.size} but curve:size: ${params.order.bitLength()}")
+        }
+        val s = BigInteger(1, magnitude)
+        if (s < BigInteger.ONE || s >= params.order) TODO("s: $s but n: ${params.order}")
+        val kf = KeyFactory.getInstance("ec")
+        return kf.generatePrivate(ECPrivateKeySpec(s, params))
+    }
+}
