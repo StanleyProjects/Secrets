4488537e1d864b2bc72188a495c239e3c7ecbda5
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-24 18:36:21 +0300

    test

---
+++ lib/src/test/kotlin/sp/kx/secrets/RSATest.kt
@@ -0,0 +1,54 @@
+package sp.kx.secrets
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.security.KeyPairGenerator
+import java.security.MessageDigest
+import java.security.SecureRandom
+
+internal object RSATest {
+    @Test
+    fun toPublicKeyTest() {
+        val kpg = KeyPairGenerator.getInstance("rsa")
+        val md = MessageDigest.getInstance("sha512")
+        var byte: Byte = -1
+        for (i in 0 until 4) {
+            md.update(byte++)
+            kpg.initialize(4096, SecureRandom(md.digest()))
+            val keyPair = kpg.generateKeyPair()
+            val expected = keyPair.public.encoded
+            assertTrue(expected.contentEquals(AsyKeys.RSA.toPublicKey(keyPair.public.encoded).encoded))
+            assertTrue(expected.contentEquals(AsyKeys.RSA.plus(keyPair.public.encoded).encoded))
+        }
+    }
+
+    @Test
+    fun toPrivateKeyTest() {
+        val kpg = KeyPairGenerator.getInstance("rsa")
+        val md = MessageDigest.getInstance("sha512")
+        var byte: Byte = -1
+        for (i in 0 until 4) {
+            md.update(byte++)
+            kpg.initialize(4096, SecureRandom(md.digest()))
+            val keyPair = kpg.generateKeyPair()
+            val expected = keyPair.private.encoded
+            assertTrue(expected.contentEquals(AsyKeys.RSA.toPrivateKey(keyPair.private.encoded).encoded))
+            assertTrue(expected.contentEquals(AsyKeys.RSA.minus(keyPair.private.encoded).encoded))
+        }
+    }
+
+    @Test
+    fun getPublicKeyTest() {
+        val kpg = KeyPairGenerator.getInstance("rsa")
+        val md = MessageDigest.getInstance("sha512")
+        var byte: Byte = -1
+        for (i in 0 until 4) {
+            md.update(byte++)
+            kpg.initialize(4096, SecureRandom(md.digest()))
+            val keyPair = kpg.generateKeyPair()
+            val expected = keyPair.public.encoded
+            val actual = AsyKeys.RSA.getPublicKey(keyPair.private).encoded
+            assertTrue(expected.contentEquals(actual))
+        }
+    }
+}
